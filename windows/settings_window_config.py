98f942aa6b713bc9220454f330089a8529f2110c
Timur <timur9165489418@mail.ru>
2022-05-05 22:56:07 +0300

    Дополнена проверка правильности заполнения полей. Пользователю показываются неверно заполненные поля. Теперь конфиг создается, если его не было изначально

--- windows/settings_window_config.py
+++ windows/settings_window_config.py
@@ -1,6 +1,7 @@
 import os
 import json
 from windows.settings_window import *
+import re
 
 
 class Monitor(QtCore.QThread):
@@ -24,13 +25,13 @@ class Settings(QtWidgets.QWidget):
         self.setting.pushButton_6.clicked.connect(self.save_function)
         self.setting.pushButton_7.clicked.connect(lambda: self.close())
 
-        #Подргузка данных из .json
+        # Подргузка данных из .json
         if os.path.exists(os.path.join("saved_parameters.json")):
             with open(os.path.join("saved_parameters.json")) as file:
                 data = json.load(file)
                 self.setting.lineEdit_2.setText(data['server_ip'])
                 self.setting.lineEdit_3.setText(data['server_port'])
-                self.setting.lineEdit_4.setText(data['nickname'])
+                self.setting.lineEdit_4.setText(data['nick'])
 
     # Функции перетаскивания, передвижения окна настроек
     def center(self):
@@ -54,13 +55,40 @@ class Settings(QtWidgets.QWidget):
     def save_function(self):
         server_ip = self.setting.lineEdit_2.text()
         server_port = self.setting.lineEdit_3.text()
-        nickname = self.setting.lineEdit_4.text()
+        nick = self.setting.lineEdit_4.text()
+        regular_ip = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
 
-        if 3 <= len(nickname) <= 20:
-            with open(os.path.join("saved_parameters.json"), "w") as file:
-                save = {"server_ip": server_ip, "server_port": server_port, "nickname": nickname}
-                json.dump(save, file)
-            # Закрываем окно с настройками
-            self.close()
+        # Показываем пользователю неверно заполненные поля
+        self.setting.lineEdit_2.setStyleSheet("border-radius: 7px;")
+        self.setting.lineEdit_3.setStyleSheet("border-radius: 7px;")
+        self.setting.lineEdit_4.setStyleSheet("border-radius: 7px;")
+
+        # Проверяем правильность заполнения полей пользователем
+        if 3 <= len(nick) <= 20:
+            if not re.match(regular_ip, self.setting.lineEdit_2.text()) is None:
+                if server_port.isdecimal() and int(server_port) <= 65535:
+
+                    # Создаем конфиг, если его еще не существовало
+                    if not os.path.exists(os.path.join("saved_parameters.json")):
+                        with open(os.path.join("saved_parameters.json"), "w") as file:
+                            data = {"nick": nick, "server_ip": server_ip, "server_port": server_port}
+                            json.dump(data, file, indent=6)
+
+                    # Если конфиг был создан ранее, то идет его перезапись
+                    else:
+                        with open(os.path.join("saved_parameters.json"), "w") as file:
+                            data = {"nick": nick, "server_ip": server_ip, "server_port": server_port}
+                            json.dump(data, file, indent=6)
+
+                        # Закрываем окно с настройками
+                    self.close()
+
+                else:
+                    self.setting.lineEdit_3.setStyleSheet("border: 2px solid red; border-radius: 7px;")
+                    self.setting.lineEdit_3.setText("Вы неверно ввели Порт")
+            else:
+                self.setting.lineEdit_2.setStyleSheet("border: 2px solid red; border-radius: 7px;")
+                self.setting.lineEdit_2.setText("Вы неверно ввели IP-адрес")
         else:
+            self.setting.lineEdit_4.setStyleSheet("border: 2px solid red; border-radius: 7px;")
             self.setting.lineEdit_4.setText("Ник должен состоять от 3 до 20 символов")
